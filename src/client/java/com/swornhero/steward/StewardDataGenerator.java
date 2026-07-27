33a1e3a25c75ea2170595baf7fe13566f63ec725
SwornHero <92185460+golso4243@users.noreply.github.com>
2026-07-27 16:26:03 -0500

    initial commit

---
+++ src/client/java/com/swornhero/steward/StewardDataGenerator.java
@@ -0,0 +1,12 @@
+package com.swornhero.steward;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jspecify.annotations.NonNull;
+
+public class StewardDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/swornhero/steward/Steward.java
@@ -0,0 +1,17 @@
+package com.swornhero.steward;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Steward implements ModInitializer {
+	public static final String MOD_ID = "steward";
+	public static final String MOD_NAME = "Steward";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("{} is initializing.", MOD_NAME);
+	}
+}
