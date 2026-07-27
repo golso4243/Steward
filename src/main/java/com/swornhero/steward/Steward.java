a90f11134839bf622b069ac3f481ee8d75622848
SwornHero <92185460+golso4243@users.noreply.github.com>
2026-07-27 18:42:51 -0500

    feat: add frozen player interaction protections

--- src/main/java/com/swornhero/steward/Steward.java
+++ src/main/java/com/swornhero/steward/Steward.java
@@ -2,6 +2,8 @@ package com.swornhero.steward;
 
 import com.swornhero.steward.command.StewardCommands;
 import com.swornhero.steward.freeze.FreezeService;
+import com.swornhero.steward.freeze.FreezeProtectionService;
+
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +20,9 @@ public final class Steward implements ModInitializer {
 
 		StewardCommands.register();
 		FreezeService.register();
+		FreezeProtectionService.register();
 
 		LOGGER.info("{} commands registered.", MOD_NAME);
-		LOGGER.info("{} freeze service registered.", MOD_NAME);
+		LOGGER.info("{} freeze services registered.", MOD_NAME);
 	}
 }
