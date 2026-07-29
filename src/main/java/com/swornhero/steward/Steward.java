5d73394918618fb8716c50e17154564086193546
SwornHero <92185460+golso4243@users.noreply.github.com>
2026-07-29 09:56:33 -0500

    refactor: add Freeze module entry point

--- src/main/java/com/swornhero/steward/Steward.java
+++ src/main/java/com/swornhero/steward/Steward.java
@@ -1,40 +1,40 @@
 package com.swornhero.steward;
 
 import com.swornhero.steward.core.command.StewardCommands;
-import com.swornhero.steward.module.freeze.service.PendingNotificationService;
-import com.swornhero.steward.module.freeze.service.FreezeService;
-import com.swornhero.steward.module.freeze.service.FreezeProtectionService;
-import com.swornhero.steward.module.freeze.service.FreezeConnectionService;
-import com.swornhero.steward.module.freeze.service.FreezeHistoryService;
-import com.swornhero.steward.module.freeze.config.FreezePolicyService;
-import com.swornhero.steward.module.freeze.service.FreezeDamageService;
-
+import com.swornhero.steward.module.freeze.FreezeModule;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Steward implements ModInitializer {
-	public static final String MOD_ID = "steward";
-	public static final String MOD_NAME = "Steward";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+	public static final String MOD_ID =
+			"steward";
+
+	public static final String MOD_NAME =
+			"Steward";
+
+	public static final Logger LOGGER =
+			LoggerFactory.getLogger(MOD_NAME);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("{} is initializing.", MOD_NAME);
-
-		FreezePolicyService.register();
-
-		FreezeHistoryService.register();
-		PendingNotificationService.register();
+		LOGGER.info(
+				"{} is initializing.",
+				MOD_NAME
+		);
 
 		StewardCommands.register();
-		FreezeService.register();
-		FreezeDamageService.register();
-		FreezeProtectionService.register();
-		FreezeConnectionService.register();
+		FreezeModule.register();
 
-		LOGGER.info("{} commands registered.", MOD_NAME);
-		LOGGER.info("{} freeze services registered.", MOD_NAME);
+		LOGGER.info(
+				"{} commands registered.",
+				MOD_NAME
+		);
+
+		LOGGER.info(
+				"{} modules registered.",
+				MOD_NAME
+		);
 	}
 }
